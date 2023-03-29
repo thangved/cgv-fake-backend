543e28fadb9ad3c1a4a75ce8832e0cd3b369b0a1
LeGiaLuat <luatb2007195@student.ctu.edu.vn>
2023-03-29 19:56:26 +0700

    update

--- models/category.model.js
+++ models/category.model.js
@@ -2,10 +2,10 @@ const sequelize = require('@/services/sequelize.service');
 const { DataTypes } = require('sequelize');
 
 
-const Country = sequelize.define('country', {
+const Category = sequelize.define('category', {
 	id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
 	name: { type: DataTypes.STRING },
 });
 
 
-module.exports = Country;
+module.exports = Category;
